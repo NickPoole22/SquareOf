52edec724d81394ebc2b73855fa94b0d9c54354b
NickPoole22 <91493463+NickPoole22@users.noreply.github.com>
2021-10-05 10:51:36 -0700

    Initial commit

---
+++ src/Application.java
@@ -0,0 +1,10 @@
+public class Application {
+
+    public static void main(String[] args) {
+        System.out.println(SquareOf(4));
+    }
+    public static int SquareOf(int num)
+    {
+        return (num * num);
+    }
+}
